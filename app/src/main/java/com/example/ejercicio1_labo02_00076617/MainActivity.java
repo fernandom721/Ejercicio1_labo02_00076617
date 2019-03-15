c9847dd60273b0a4143591875d3c1ffcbac0f3ae
fernandom721 <00076617@uca.edu.sv>
2019-03-15 09:06:43 -0600

    initial commit

---
+++ app/src/main/java/com/example/ejercicio1_labo02_00076617/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio1_labo02_00076617;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
