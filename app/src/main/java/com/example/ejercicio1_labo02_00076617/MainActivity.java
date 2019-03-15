4f52560671750738f74edcbc4fa78c2a90374c0b
fernandom721 <00076617@uca.edu.sv>
2019-03-15 10:20:56 -0600

    click and long click

--- app/src/main/java/com/example/ejercicio1_labo02_00076617/MainActivity.java
+++ app/src/main/java/com/example/ejercicio1_labo02_00076617/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.ejercicio1_labo02_00076617;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+        final EditText username = findViewById(R.id.username);
+        final EditText password = findViewById(R.id.password);
+        final TextView text = findViewById(R.id.text);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(text.getContext(),username.getText().toString(),Toast.LENGTH_LONG);
+                text.setText(username.getText());
+            }
+        });
+
+        btn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(text.getContext(),password.getText().toString(),Toast.LENGTH_LONG);
+                text.setText(password.getText());
+                return true;
+            }
+        });
     }
 }
